b4e11472dfca164f5dc648143d51c5e2d38723b0
asselbissembay <asselbiss1017@gmail.com>
2021-08-12 18:35:35 -0400

    conflict

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -7,8 +7,7 @@ public class ClassOne {
         System.out.println("Hello World");
         System.out.println("Hello today");
         System.out.println("Hi everyone");
+        System.out.println("Here is a new branch");
+        System.out.println("conflicting line");
     }
-    System.out.println("Hello World");
-    System.out.println("Here is a new branch");
-
 }
